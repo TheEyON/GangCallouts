03db335c224ef814eee23b2fcfb0df321b017806
Eyon <Eyon@AERIAL-PC>
2017-04-25 21:39:57 +0200

    Good

--- GangCallouts/CommonStuff/gangPeds.cs
+++ GangCallouts/CommonStuff/gangPeds.cs
@@ -12,13 +12,13 @@ namespace GangCallouts.CommonStuff
         // Families
         string[] families = new string[] 
             {
-            "g_f_y_families_01", "g_m_y_famca_01", "g_m_y_famdnf_01", "g_m_y_famfor_01", "g_m_y_strpunk_02", "csb_ramp_gang", "ig_lamardavis"
+            "g_f_y_families_01", "g_m_y_famca_01", "g_m_y_famdnf_01", "g_m_y_famfor_01", "g_m_y_strpunk_02", "csb_ramp_gang"
             };
         //-----------------------------------------------------------------------------------
         // Ballas
-        string[] Ballas = new string[]
+        string[] ballas = new string[]
             {
-            "g_f_y_ballas_01", "g_m_y_ballaeast_01", "g_m_y_ballaorig_01", "g_m_y_ballasout_01", "g_m_y_strpunk_01", "csb_ballasog", "ig_stretch"
+            "g_f_y_ballas_01", "g_m_y_ballaeast_01", "g_m_y_ballaorig_01", "g_m_y_ballasout_01", "g_m_y_strpunk_01", "csb_ballasog"
             };
         //-----------------------------------------------------------------------------------
         // Vagos
@@ -36,10 +36,9 @@ namespace GangCallouts.CommonStuff
             };
         //-----------------------------------------------------------------------------------
         // Rednecks
-        string[] vagos = new string[]
+        string[] rednecks = new string[]
             {
-            "g_f_y_vagos_01", "g_m_y_azteca_01", "g_m_y_pologoon_02", "g_m_y_salvaboss_01", "g_m_y_salvagoon_01", "g_m_y_salvagoon_02",
-            "g_m_y_salvagoon_03", "csb_ortega"
+            "a_m_y_acult_01", "a_m_o_acult_02", "a_m_o_beach_01", "a_m_m_rurmeth_01", "a_m_m_salton_01", "a_m_m_salton_02", "a_m_m_salton_03", "a_m_m_salton_04"
             };
         //-----------------------------------------------------------------------------------
     }
