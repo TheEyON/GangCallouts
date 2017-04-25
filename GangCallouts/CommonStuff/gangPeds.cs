12595ec2442c64ad7c4ea950019790b993cd3607
Aerial <Aerial@AERIALLAPTOP>
2017-04-25 17:49:13 +0200

    Config rednecks

---
+++ GangCallouts/CommonStuff/gangPeds.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GangCallouts.CommonStuff
+{
+    class gangPeds
+    {
+        //-----------------------------------------------------------------------------------
+        // Families
+        string[] families = new string[] 
+            {
+            "g_f_y_families_01", "g_m_y_famca_01", "g_m_y_famdnf_01", "g_m_y_famfor_01", "g_m_y_strpunk_02", "csb_ramp_gang", "ig_lamardavis"
+            };
+        //-----------------------------------------------------------------------------------
+        // Ballas
+        string[] Ballas = new string[]
+            {
+            "g_f_y_ballas_01", "g_m_y_ballaeast_01", "g_m_y_ballaorig_01", "g_m_y_ballasout_01", "g_m_y_strpunk_01", "csb_ballasog", "ig_stretch"
+            };
+        //-----------------------------------------------------------------------------------
+        // Vagos
+        string[] vagos = new string[]
+            {
+            "g_f_y_vagos_01", "g_m_y_azteca_01", "g_m_y_pologoon_02", "g_m_y_salvaboss_01", "g_m_y_salvagoon_01", "g_m_y_salvagoon_02",
+            "g_m_y_salvagoon_03", "csb_ortega"
+            };
+        //-----------------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------------
+        // Losts
+        string[] losts = new string[]
+            {
+            "g_f_y_lost_01", "g_m_y_lost_01", "g_m_y_lost_02", "g_m_y_lost_03"
+            };
+        //-----------------------------------------------------------------------------------
+        // Rednecks
+        string[] vagos = new string[]
+            {
+            "g_f_y_vagos_01", "g_m_y_azteca_01", "g_m_y_pologoon_02", "g_m_y_salvaboss_01", "g_m_y_salvagoon_01", "g_m_y_salvagoon_02",
+            "g_m_y_salvagoon_03", "csb_ortega"
+            };
+        //-----------------------------------------------------------------------------------
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Documentation: 
+
+    switch (faction)
+            {
+                case "families":
+                    break;
+                    
+                case "ballas":
+                    break;
+                    
+                case "vagos":
+                    break;
+
+                case "losts":
+                    break;
+
+                case "aztecas":
+                    break;
+
+                case "rednecks":
+                    break;
+
+                case "marabunta":
+                    break;
+
+                case "triadmafia":
+                    break;
+
+                case "cartelmafia":
+                    break;
+
+                case "eeumafia":
+                    break;
+
+                case "koreanmafia":
+                    break;
+
+                default:
+                    break;
+            }
+
+    */
