3dac17d471299db283748fb6b6f1aa7b857debb3
Eyon <Eyon@AERIAL-PC>
2017-04-25 22:41:32 +0200

    Ajout des références & ressources

--- GangCallouts/CommonStuff/mafiaPeds.cs
+++ GangCallouts/CommonStuff/mafiaPeds.cs
@@ -9,28 +9,27 @@ namespace GangCallouts.CommonStuff
     class mafiaPeds
     {
         //-----------------------------------------------------------------------------------
-        // Families
-        string[] families = new string[]
+        // Triads
+        string[] triads = new string[]
             {
             "g_f_y_families_01", "g_m_y_famca_01", "g_m_y_famdnf_01", "g_m_y_famfor_01", "g_m_y_strpunk_02", "csb_ramp_gang", "ig_lamardavis"
             };
         //-----------------------------------------------------------------------------------
-        // Ballas
-        string[] Ballas = new string[]
+        // Cartel
+        string[] cartel = new string[]
             {
             "g_f_y_ballas_01", "g_m_y_ballaeast_01", "g_m_y_ballaorig_01", "g_m_y_ballasout_01", "g_m_y_strpunk_01", "csb_ballasog", "ig_stretch"
             };
         //-----------------------------------------------------------------------------------
-        // Vagos
-        string[] vagos = new string[]
+        // Armenian
+        string[] armenian = new string[]
             {
             "g_f_y_vagos_01", "g_m_y_azteca_01", "g_m_y_pologoon_02", "g_m_y_salvaboss_01", "g_m_y_salvagoon_01", "g_m_y_salvagoon_02",
             "g_m_y_salvagoon_03", "csb_ortega"
             };
-        //-----------------------------------------------------------------------------------
-        //-----------------------------------------------------------------------------------
-        // Losts
-        string[] losts = new string[]
+       //-----------------------------------------------------------------------------------
+        // Korean
+        string[] korean = new string[]
             {
             "g_f_y_lost_01", "g_m_y_lost_01", "g_m_y_lost_02", "g_m_y_lost_03"
             };
